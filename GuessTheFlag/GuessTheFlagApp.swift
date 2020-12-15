2eeb442b2f04c110fffb6882b5c3ad5a0da8598d
Nicolas Papegaey <nicolas.papegaey@gmail.com>
2020-12-15 11:27:19 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nicolas Papegaey on 15/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
